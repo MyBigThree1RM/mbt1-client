ea76c9f79ee629b1170e2485859f93b310f2f729
한은영 <han-eun-yeong@han-eun-yeong-ui-MacBookAir.local>
2022-04-06 00:17:14 +0900

    add camera component

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import { Camera } from 'expo-camera';
 import { theme } from "./colors";
 
 const Stack = createStackNavigator();
@@ -11,6 +12,22 @@ function Measure({navigation}){
   const [weight, setWeight] = useState(0);
   const [reps, setReps] = useState(0);
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
 
   const paintWeight = () => {
     if (weight<=0) {
@@ -29,9 +46,21 @@ function Measure({navigation}){
 
   return(
     <View style={styles.container}>
-      <View style={{...styles.container,flex:3,width:"100%",backgroundColor:"black"}}>
-        <Text style={{color:"white"}}>Video Stream</Text>
-      </View>
+      <Camera style={styles.camera} type={type}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}>
+            <Text style={styles.text}> Flip </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
 
       <View style={styles.repBox}>
         <View style={{flex:0.7, flexDirection:"row",alignItems:"center"}}>
@@ -101,7 +130,7 @@ const styles = StyleSheet.create({
     fontWeight:"500"
   },
   repBox:{
-    flex:0.8,
+    flex:0.2,
     width:"90%",
     borderRadius:20,
     justifyContent:"center",
@@ -120,7 +149,7 @@ const styles = StyleSheet.create({
     fontSize:18
   },
   submit:{
-    flex:0.2,
+    flex:0.05,
     width:"90%",
     alignItems:"center",
     justifyContent:"center",
@@ -133,5 +162,24 @@ const styles = StyleSheet.create({
     fontSize:20,
     color:"white",
     fontWeight:"600",
+  },
+  camera: {
+    width:"100%",
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    margin: 20,
+  },
+  button: {
+    flex: 0.1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    color: 'white',
   },
 });
